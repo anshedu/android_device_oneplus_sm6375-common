Realme-SM6375-devs/android_device_realme_sm6375-common

sm6375-common: Include makefiles from common proprietary repo
Change-Id: Id1d1d4f636e029cc6d147dafd0b838480d5dbc57

BoardConfigCommon.mk
@@ -55,3 +55,6 @@ TARGET_BOARD_PLATFORM := holi
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
+
+# Inherit the proprietary files
+include vendor/realme/sm6375-common/BoardConfigVendor.mk

sm6375.mk
@@ -16,3 +16,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/realme/sm6375-common/sm6375-common-vendor.mk)

anshedu/android_device_oneplus_sm6375-common

BoardConfigCommon.mk
@@ -55,3 +55,6 @@ TARGET_BOARD_PLATFORM := holi
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
+
+# Inherit the proprietary files
+include vendor/oneplus/sm6375-common/BoardConfigVendor.mk

sm6375.mk
@@ -16,3 +16,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/oneplus/sm6375-common/sm6375-common-vendor.mk)