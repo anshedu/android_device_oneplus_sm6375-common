Realme-SM6375-devs/android_device_realme_sm6375-common

sm6375-common: Switch to py extract utils
Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

extract-files.py
@@ -1,0 +1,82 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/realme/sm6375-common',
+    'hardware/oplus',
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}-vendor' if partition in ['odm', 'vendor'] else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/lib/liblvimfs_wrapper.so': blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
+        .regex_replace('/my_product', '/product'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
+    'vendor/etc/media_holi/video_system_specs.json': blob_fixup()
+        .regex_replace('"max_retry_alloc_output_timeout": 2000,', '"max_retry_alloc_output_timeout": 0,'),
+    ('vendor/etc/media_codecs_blair.xml', 'vendor/etc/media_codecs_holi.xml'): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).*\n', ''),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm6375-common',
+    'realme',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

anshedu/android_device_oneplus_sm6375-common

extract-files.py
@@ -1,0 +1,82 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/oneplus/sm6375-common',
+    'hardware/oplus',
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}-vendor' if partition in ['odm', 'vendor'] else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/lib/liblvimfs_wrapper.so': blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
+        .regex_replace('/my_product', '/product'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
+    'vendor/etc/media_holi/video_system_specs.json': blob_fixup()
+        .regex_replace('"max_retry_alloc_output_timeout": 2000,', '"max_retry_alloc_output_timeout": 0,'),
+    ('vendor/etc/media_codecs_blair.xml', 'vendor/etc/media_codecs_holi.xml'): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).*\n', ''),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm6375-common',
+    'oneplus',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()