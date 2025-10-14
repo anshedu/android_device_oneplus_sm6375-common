62248b74bc5bc01c51fdc29ee9abe74703dda786
Vivekachooz <vivekachooz@gmail.com>
2025-10-14 23:31:45 +0530

    sm6375-common: Update from CPH2467_15.0.0.1360(EX01)
    Change-Id: I7ad0dc569695e4de55e2a6c5b94352c5b649ed15

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,7 +163,7 @@ include hardware/oplus/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-BOOT_SECURITY_PATCH := 2025-09-01
+BOOT_SECURITY_PATCH := 2025-10-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Verified Boot
